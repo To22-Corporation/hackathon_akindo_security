76501d1fceb17076f95ed7aa8057fefb9086c89c
zenrshon <inosun@suou.waseda.jp>
2022-10-30 17:28:18 +0900

    BSC、Polygonのscam検出を追加

--- back/scamCheck/scanscam.js
+++ back/scamCheck/scanscam.js
@@ -2,7 +2,7 @@ var fileSystem = require("fs");
 
 scanscamtoken = async function (contractAddress, network) {
     const result = await new Promise((resolve, reject) => {
-        fileSystem.readFile(`./scamCheck/${network}_parsedscamremoveduplicate.txt`, 'utf-8', function (error, data) {
+        fileSystem.readFile(`./scamCheck/${network}_scam_token.txt`, 'utf-8', function (error, data) {
             if (error) throw error;
             else {
                 parsedData = data.toString().replace(/\r/g, "").split("\n");
--- back/scamCheck/scanscamaddress.js
+++ back/scamCheck/scanscamaddress.js
@@ -2,7 +2,7 @@ var fileSystem = require("fs");
 
 scanscamaddress = async function (contractAddress, network) {
     const result = await new Promise((resolve, reject) => {
-        fileSystem.readFile(`./scamCheck/${network}_parsedscamaddressremoveduplicate.txt`, 'utf-8', function (error, data) {
+        fileSystem.readFile(`./scamCheck/${network}_scam_address.txt`, 'utf-8', function (error, data) {
             if (error) throw error;
             else {
                 parsedData = data.toString().replace(/\r/g, "").split("\n");
