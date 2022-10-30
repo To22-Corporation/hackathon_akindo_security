83392fdb532ee73ab6820739896efd967879aea2
zenrshon <inosun@suou.waseda.jp>
2022-10-30 13:05:48 +0900

    テストレスポンス名の変更

--- index.js
+++ index.js
@@ -41,21 +41,21 @@ app.post("/erc20Test", async (req, res) => {
   console.log(result)
 
   const resultResponse = {
-    result1: result.includes("✔ name should return a string value"),
-    result2: result.includes("✔ symbol should return a string value"),
-    result3: result.includes("✔ decimal should return a number"),
-    result4: result.includes("✔ balanceOf should return correct amount for each user"),
-    result5: result.includes("✔ transfer cannot be used by owner that does not have tokens"),
-    result6: result.includes("✔ transfer cannot be used by user that does not have enough tokens"),
-    result7: result.includes("✔ transfer can be used by user that has enough tokens 1"),
-    result8: result.includes("✔ transfer can be used by user that has enough tokens 2"),
-    result9: result.includes("✔ allowance returns 0 when not approved"),
-    result10: result.includes("✔ allowance returns amount when not approved"),
-    result11: result.includes("✔ approve user can spend token 1"),
-    result12: result.includes("✔ approve user can spend token 2"),
-    result13: result.includes("✔ approve user can spend token 3"),
-    result14: result.includes("✔ transferFrom cannot be used by owner that does not have tokens"),
-    result15: result.includes("✔ transferFrom cannot be used by user that does not have enough tokens")
+    result_contract_name: result.includes("✔ name should return a string value"),
+    result_symbol_name: result.includes("✔ symbol should return a string value"),
+    result_decimal: result.includes("✔ decimal should return a number"),
+    result_balance: result.includes("✔ balanceOf should return correct amount for each user"),
+    result_cant_transfer_by_owner: result.includes("✔ transfer cannot be used by owner that does not have tokens"),
+    result_cant_transfer_by_user: result.includes("✔ transfer cannot be used by user that does not have enough tokens"),
+    result_transfer_1: result.includes("✔ transfer can be used by user that has enough tokens 1"),
+    result_transfer_2: result.includes("✔ transfer can be used by user that has enough tokens 2"),
+    result_not_approve_0: result.includes("✔ allowance returns 0 when not approved"),
+    result_not_approve_amount: result.includes("✔ allowance returns amount when not approved"),
+    result_approve_user_1: result.includes("✔ approve user can spend token 1"),
+    result_approve_user_2: result.includes("✔ approve user can spend token 2"),
+    result_approve_user_3: result.includes("✔ approve user can spend token 3"),
+    result_transferfrom_owner: result.includes("✔ transferFrom cannot be used by owner that does not have tokens"),
+    result_transferfrom_user: result.includes("✔ transferFrom cannot be used by user that does not have enough tokens")
   }
   const count = (JSON.stringify(resultResponse).match(/true/g) || [])
   resultResponse.score = Math.round(count.length / 15 * 100)
