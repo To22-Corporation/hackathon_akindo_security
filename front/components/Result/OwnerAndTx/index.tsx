0eae74f58e73129a454740b36e687f6ce5029b46
zenrshon <inosun@suou.waseda.jp>
2022-11-06 12:04:05 +0900

    score & readme fix

--- front/components/Result/OwnerAndTx/index.tsx
+++ front/components/Result/OwnerAndTx/index.tsx
@@ -5,10 +5,21 @@ type Props = {
   data: any;
 };
 export const OwnerAndTx: React.FC<Props> = (props) => {
+  const Message: React.FC<any> = (props: any) => {
+    if (props.score == 100 || props.score == 80) {
+      return <p>トークン保有者は分散しています</p>;
+    } else if (props.score == 60) {
+      return <p>トークンはある程度分散しています</p>;
+    } else if (props.core == 40 || props.score == 20) {
+      return <p>トークン保有者にやや偏りがあります</p>;
+    } else {
+      return <p>トークン保有者に偏りがあります</p>;
+    }
+  };
   return (
     <Box>
       <Text fontWeight="bold" fontSize="28px" textAlign="center">
-        上位20人が全体の約{props.data.score}%のトークンを保有しています
+        <Message score={props.data.score} />
       </Text>
     </Box>
   );
